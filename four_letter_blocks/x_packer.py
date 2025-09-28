aff8618c9d43706ef0d346b7ee9f411ef8725443
Don Kirkby <donkirkby@gmail.com>
2025-09-28 11:19:41 -0700

    Keep shape counts roughly even, for #56.

--- four_letter_blocks/x_packer.py
+++ four_letter_blocks/x_packer.py
@@ -1,7 +1,10 @@
+import typing
+
 import numpy as np
-from miniexact import miniexacts_x
+from miniexact import miniexacts_m
 
 from four_letter_blocks.block_packer import BlockPacker, build_masks
+from four_letter_blocks.puzzle import Puzzle
 
 SOLUTION_FOUND = 10
 SOLUTION_NOT_FOUND = 20
@@ -20,9 +23,6 @@ class XPacker(BlockPacker):
         self.are_slots_shuffled is True, then coverage ties are broken randomly,
         otherwise ties are filled from top to bottom.
 
-        If self.are_partials_saved is True, then we don't cycle through options,
-        just make the first choice for each slot and return with self.state set.
-
         For self.target_shape_counts and self.required_shape_counts, disables
         rotation if any of the shapes contain a letter and rotation number. They
         are adjusted to remaining counts, if self.are_partials_saved is True. If
@@ -31,18 +31,30 @@ class XPacker(BlockPacker):
         :return: True, if self.required_shape_counts has gone to zero, or if
             it's None and no gaps are left, otherwise False.
         """
+        for state in self.find_fillings():
+            self.state = state
+            return True
+        return False
+
+    def find_fillings(self) -> typing.Iterator[np.ndarray]:
         width = self.width
         height = self.height
+        start_state = self.state
+        all_word_items: list[set[str]] = self.find_word_items()
         # items are spaces in the grid, converted to an integer as
         # row*width + column.
         # Each option is a slot taking up four spaces.
-        solver = miniexacts_x()
+        solver = miniexacts_m()
         open_spaces = [
             (int(i), int(j))
             for i, j in zip(*np.where(self.state == self.UNUSED))]
         for i, j in open_spaces:
             item_name = f'{i},{j}'
             solver.primary(item_name)
+        max_shape_counts = self.calculate_max_shape_counts()
+        for shape_name, max_shape_count in max_shape_counts.items():
+            min_shape_count = max((max_shape_count-1) // 2, 0)
+            solver.primary(shape_name, min_shape_count, max_shape_count)
 
         slots = self.find_slots()
         option_masks = {}  # { option_num: [[flag]] }
@@ -56,19 +68,55 @@ class XPacker(BlockPacker):
                         covered_spaces = [
                             (int(i2), int(j2))
                             for i2, j2 in zip(*np.where(coverage_flags))]
-                        for i2, j2 in covered_spaces:
-                            item_name = f'{i2},{j2}'
+                        block_items = {f'{i2},{j2}'
+                                       for i2, j2 in covered_spaces}
+                        has_complete_word = any(
+                            word_items.issubset(block_items)
+                            for word_items in all_word_items
+                        )
+                        if has_complete_word:
+                            continue
+
+                        for item_name in block_items:
                             solver.add(item_name)
+                        solver.add(shape_name)
                         option_num = solver.add(0)  # Finish option
                         option_masks[option_num] = coverage_flags
-        result = solver.solve()
-        if result == SOLUTION_NOT_FOUND:
-            return False
-        assert result == SOLUTION_FOUND
-        selected_options = solver.selected_options()
-        start_block = max(int(self.state.max()), self.GAP) + 1
-        for block_num, option_num in enumerate(selected_options, start_block):
-            coverage_flags = option_masks[option_num][:width, :height]
-            self.state += np.uint8(block_num) * coverage_flags
-        return True
+        while solver.solve() == SOLUTION_FOUND:
+            selected_options = solver.selected_options()
+            new_state = start_state.copy()
+            start_block = max(int(new_state.max()), self.GAP) + 1
+            for block_num, option_num in enumerate(selected_options, start_block):
+                coverage_flags = option_masks[option_num][:width, :height]
+                new_state += np.uint8(block_num) * coverage_flags
+            yield new_state
 
+    def find_word_items(self) -> list[set[str]]:
+        all_word_items: list[set[str]] = []
+        start_text = self.display()
+        puzzle = Puzzle.parse_sections('',
+                                       start_text.replace('.', 'X'),
+                                       '',
+                                       start_text)
+        for i in range(self.height):
+            y = i + 1  # Don't remember why the grid uses 1-based x and y.
+            for j in range(self.width):
+                x = j + 1
+                square = puzzle.grid.squares[y][x]
+                if square is None:
+                    continue
+                for word, di, dj in ((square.across_word, 0, 1),
+                                     (square.down_word, 1, 0)):
+                    if word is None:
+                        continue
+                    if len(word) > 4:
+                        continue
+                    word_items = []
+                    for k in range(len(word)):
+                        i2 = i + k*di
+                        j2 = j + k*dj
+                        item_name = f'{i2},{j2}'
+                        word_items.append(item_name)
+                    word_item_set = set(word_items)
+                    all_word_items.append(word_item_set)
+        return all_word_items
--- tests/test_x_packer.py
+++ tests/test_x_packer.py
@@ -5,29 +5,30 @@ from four_letter_blocks.x_packer import XPacker
 
 
 def test_fill_grid():
-    shape_counts = Counter('I')
     start_text = dedent("""\
-        #...#
-        .....
-        ..#..
-        .....
-        #...#""")
+        #.....#
+        ...#...
+        .......
+        .#.#.#.
+        .......
+        ...#...
+        #.....#""")
     expected_display = dedent("""\
-        #AEE#
-        AAEED
-        BA#DD
-        BBBCD
-        #CCC#""")
+        #AACCC#
+        BAA#CJJ
+        BBFFJJI
+        B#F#G#I
+        DDFGGII
+        DDE#GHH
+        #EEEHH#""")
     packer = XPacker(start_text=start_text)
-    packer.required_shape_counts = shape_counts
     is_filled = packer.fill()
 
-    assert packer.display() == expected_display
     assert is_filled
+    assert packer.display() == expected_display
 
 
 def test_fill_big_grid():
-    shape_counts = Counter('I')
     start_text = dedent("""\
         .#...........#.
         ...##.....##...
@@ -45,23 +46,22 @@ def test_fill_big_grid():
         ...##.....##...
         .#...........#.""")
     expected_display = dedent("""\
-        A#CDDDDaaaOOO#L
-        AAC##````a##OLL
-        ACCE#___bb#NNNL
-        BBBEE#]_b#WNMMM
-        B##E]]]#bWWW##M
-        #ZX[[[^^ccdddj#
-        ZZX#[^^#ccd#hjj
-        YZXX#T###e#hhij
-        YYY#TTT#Vee#hii
-        #SSSRRVVVegggi#
-        K##SRUU#fffg##G
-        KKQQR#UUk#fHGGG
-        JKQQ#lkkkm#HHHF
-        JJP##llmmm##IFF
-        J#PPPlnnnnIII#F""")
+        A#CCEEEE]IIII#F
+        ACC##ZZ[]]##HHF
+        AADD#ZZ[^]#HHFF
+        BBBDS#[[^#__GGG
+        B##DSSS#^^__##G
+        #TWWWffeedddbb#
+        TTV#Wff#eed#`bb
+        TUVV#g###j#```a
+        UUV#Xgh#ijj#caa
+        #UXXXghiiijcca#
+        K##YYgh#kkkc##O
+        KKKYY#hmk#QQOOO
+        JJLL#nmmll#QQNN
+        JLL##nnmll##PPN
+        J#MMMMnRRRRPP#N""")
     packer = XPacker(start_text=start_text)
-    packer.required_shape_counts = shape_counts
     is_filled = packer.fill()
 
     assert is_filled
